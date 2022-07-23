cbafc87d0ad643930345d528d461d9b74aa4d61f
izar06 <izar.anam22@gmail.com>
2022-07-23 14:58:59 +0700

    portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,54 @@
+//burger
+
+const burger = document.querySelector('#burger');
+const navMenu = document.querySelector('#nav-menu');
+
+burger.addEventListener('click', function(){
+    burger.classList.toggle('burger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+// navbar fixed
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    }else{
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+};
+
+//click di luar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != burger && e.target != navMenu){
+        burger.classList.remove('burger-active');
+        navMenu.classList.add('hidden');
+    };
+});
+
+//dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark'
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light'
+    };
+});
+
+//ubah posisi toggle 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
